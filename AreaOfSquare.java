194d2269f716091a1c1bf854593e20ff01c5869f
Yashaswini7274 <yashaswini7274@gmail.com>
2026-01-29 20:40:50 +0530

    Create AreaOfSquare.java

---
+++ AreaOfSquare.java
@@ -0,0 +1,10 @@
+public class AreaOfSquare {
+    public static void main(String[] args) {
+
+        int side = 5;   // inbuilt input (you can change this value)
+
+        int area = side * side;
+
+        System.out.println("Area of the square is: " + area);
+    }
+}
